d1c465091ee39e92839305df0a5de915b38cffd1
Matt Haliski <matt@leavingharbor.com>
2015-07-01 11:47:33 -0600

    Homepage polish

--- app/helpers/social_helper.rb
+++ app/helpers/social_helper.rb
@@ -1,2 +1,13 @@
 module SocialHelper
+
+  # You may find the need to have a corrected :provider parameter to
+  # user_omniauth_authorize_path. E.g. "Linkedin" to "linked_in"
+  def provider_corrector(provider)
+    if provider.downcase == "linkedin"
+      "linked_in".to_sym
+    else
+      provider.downcase.to_sym
+    end
+  end
+
 end
